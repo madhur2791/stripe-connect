75f58789bb548311cba4a7096e97c925da40b431
Madhu R <madhu@casaone.com>
2018-06-04 20:11:01 +0530

    1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+$con = mysqli_connect("mysql://mysql:3306/","test_db_connection","test_db_connection_pwd","test_db");
 
-echo 'Hi';
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+	else {
+		echo 'connected';
+	}
+?>
